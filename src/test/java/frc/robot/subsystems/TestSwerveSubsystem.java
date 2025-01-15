7cbaf061faf863dcf09fd138008b1333d854894f
smjones1985 <smjones.virj@gmail.com>
2025-01-14 18:48:21 -0600

    Update TestSwerveSubsystem.java

--- src/test/java/frc/robot/subsystems/TestSwerveSubsystem.java
+++ src/test/java/frc/robot/subsystems/TestSwerveSubsystem.java
@@ -1,22 +1,24 @@
 package frc.robot.subsystems;
 
+import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.Test;
 
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.Pigeon2;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.Angle;
 
 public class TestSwerveSubsystem {
     @Test
     public void testExampleCondition(){
         Pigeon2 pigeon = mock();
-        StatusSignal statusSignal = mock();
-        when(statusSignal.getValueAsDouble()).thenAnswer(0.0);
-        when(pigeon.getYaw()).then(StatusSignal.)
+        //needs to use typeparameter Angle to align with getYaw
+        StatusSignal<Angle> statusSignal = mock();
+        // we needed to use thenReturn 
+        when(statusSignal.getValueAsDouble()).thenReturn(0.0);
+        when(pigeon.getYaw()).thenReturn(statusSignal);
         var swerveSubsystem = new SwerveSubsystem(pigeon);
 
         swerveSubsystem.isAtAngle(new Rotation2d());
