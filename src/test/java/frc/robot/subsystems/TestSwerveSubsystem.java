6bc32e86b2dd1ce7e61f0f693e41fef97bf423b2
Charlie Stein <150862516+Charlie-Stein@users.noreply.github.com>
2025-01-18 09:30:30 -0600

    Finished testIsAtAngle

--- src/test/java/frc/robot/subsystems/TestSwerveSubsystem.java
+++ src/test/java/frc/robot/subsystems/TestSwerveSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -13,18 +14,21 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.Angle;
 
 public class TestSwerveSubsystem {
+    Pigeon2 pigeon = mock();
+    //needs to use typeparameter Angle to align with getYaw
+    StatusSignal<Angle> statusSignal = mock();
     @Test
-    public void testExampleCondition(){
-        Pigeon2 pigeon = mock();
-        //needs to use typeparameter Angle to align with getYaw
-        StatusSignal<Angle> statusSignal = mock();
-        // we needed to use thenReturn 
+    public void testIsAtAngle(){
+        // we needed to use thenReturn
         when(statusSignal.getValueAsDouble()).thenReturn(0.0);
         when(pigeon.getYaw()).thenReturn(statusSignal);
         var swerveSubsystem = new SwerveSubsystem(pigeon);
         assertTrue(swerveSubsystem.isAtAngle(new Rotation2d()));
-        when(statusSignal.getValueAsDouble()).thenReturn(0.1);
+        when(statusSignal.getValueAsDouble()).thenReturn(1.6);
         assertFalse(swerveSubsystem.isAtAngle(new Rotation2d()));
-        //swerveSubsystem.isAtAngle(new Rotation2d());
+    }
+    @Test
+    public void testIsAtPoint(){
+
     }
 }
