0cb100876f054285d2615a3b72e773dc40d4be96
Charlie Stein <150862516+Charlie-Stein@users.noreply.github.com>
2025-01-18 12:07:10 -0600

    Finished TestOdometryAngle and Tidied up code

--- src/test/java/frc/robot/subsystems/TestSwerveSubsystem.java
+++ src/test/java/frc/robot/subsystems/TestSwerveSubsystem.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -11,24 +13,90 @@ import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.Pigeon2;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.units.measure.Angle;
+import frc.robot.extras.SwerveModule;
 
 public class TestSwerveSubsystem {
-    Pigeon2 pigeon = mock();
+    Pigeon2 pigeon;
     //needs to use typeparameter Angle to align with getYaw
-    StatusSignal<Angle> statusSignal = mock();
+    StatusSignal<Angle> yawStatusSignal;
+    StatusSignal<Angle> pitchStatusSignal;
+    StatusSignal<Angle> rollStatusSignal;
+    private SwerveSubsystem swerveSubsystem;
+    private GenericEntry headingShuffleBoard;
+    private GenericEntry odometerShuffleBoard;
+    private GenericEntry rollSB;
+    private GenericEntry pitchSB;
+    /*
+    private SwerveModule frontLeft;
+    private SwerveModule frontRight;
+    private SwerveModule backLeft;
+    private SwerveModule backRight;
+    */
+
+    @BeforeEach
+    public void setUp() {
+        headingShuffleBoard = mock();
+        odometerShuffleBoard = mock();
+        rollSB = mock();
+        pitchSB = mock();
+
+        pigeon = mock();
+        yawStatusSignal = mock();
+        when(pigeon.getYaw()).thenReturn(yawStatusSignal);
+        pitchStatusSignal = mock();
+        when(pigeon.getPitch()).thenReturn(pitchStatusSignal);
+        rollStatusSignal = mock();
+        when(pigeon.getRoll()).thenReturn(rollStatusSignal);
+
+        /*
+        frontLeft = mock();
+        frontRight = mock();
+        backLeft = mock();
+        backRight = mock();
+        */
+
+        swerveSubsystem = new SwerveSubsystem(pigeon, 
+        //frontLeft, frontRight, backLeft, backRight, 
+        headingShuffleBoard, odometerShuffleBoard, rollSB, pitchSB);
+    }
     @Test
     public void testIsAtAngle(){
         // we needed to use thenReturn
-        when(statusSignal.getValueAsDouble()).thenReturn(0.0);
-        when(pigeon.getYaw()).thenReturn(statusSignal);
-        var swerveSubsystem = new SwerveSubsystem(pigeon);
+        when(yawStatusSignal.getValueAsDouble()).thenReturn(0.0);
         assertTrue(swerveSubsystem.isAtAngle(new Rotation2d()));
-        when(statusSignal.getValueAsDouble()).thenReturn(1.6);
+        when(yawStatusSignal.getValueAsDouble()).thenReturn(1.6);
         assertFalse(swerveSubsystem.isAtAngle(new Rotation2d()));
     }
     @Test
     public void testIsAtPoint(){
-
+        Translation2d targetDrivePos = new Translation2d();
+        assertTrue(swerveSubsystem.isAtPoint(targetDrivePos));
+        targetDrivePos = new Translation2d(1,1);
+        assertFalse(swerveSubsystem.isAtPoint(targetDrivePos));
     }
+    @Test
+    public void testOdometryAngle(){
+        when(yawStatusSignal.getValueAsDouble()).thenReturn(90.0);
+        when(pitchStatusSignal.getValueAsDouble()).thenReturn(0.0);
+        when(rollStatusSignal.getValueAsDouble()).thenReturn(0.0);
+        swerveSubsystem.periodic();
+        var odometerPostUpdate = swerveSubsystem.getOdometer().getPoseMeters();
+        assertEquals(90.0, odometerPostUpdate.getRotation().getDegrees());
+    }
+    //TODO: testOdometryTranslation
+    /*
+    @Test
+    public void testOdometryTranslation(){
+        when(yawStatusSignal.getValueAsDouble()).thenReturn(0.0);
+        when(pitchStatusSignal.getValueAsDouble()).thenReturn(0.0);
+        when(rollStatusSignal.getValueAsDouble()).thenReturn(0.0);
+        swerveSubsystem.periodic();
+        var odometerPostUpdate = swerveSubsystem.getOdometer().getPoseMeters();
+        assertEquals(0.0, odometerPostUpdate.getTranslation().getX());
+        assertEquals(0.0, odometerPostUpdate.getTranslation().getY());
+    }
+    */
 }
