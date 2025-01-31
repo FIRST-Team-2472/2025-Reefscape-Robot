ae73c49b3c485ced74da6f5840b522ab3792a8ee
smjones1985 <smjones.virj@gmail.com>
2025-01-30 19:24:07 -0600

    format

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,57 +5,51 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
-
-import frc.robot.subsystems.CoralDispenserSubsystem;
 import frc.robot.commands.defaultCommands.ElevatorCommand;
 import frc.robot.commands.defaultCommands.SwerveJoystickCmd;
-
+import frc.robot.subsystems.CoralDispenserSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
-import edu.wpi.first.wpilibj.Joystick;
-
 
 public class RobotContainer {
-  //Add subsystems below this comment
+  // Add subsystems below this comment
   private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
 
   ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
   CoralDispenserSubsystem coralDispenserSubsytem = new CoralDispenserSubsystem();
 
-  //Make sure this xbox controller is correct and add driver sticks
+  // Make sure this xbox controller is correct and add driver sticks
   XboxController xboxController = new XboxController(OperatorConstants.kXboxControllerPort);
-
 
   public static Joystick leftJoystick = new Joystick(OperatorConstants.kLeftJoystickPort);
   public static Joystick rightJoystick = new Joystick(OperatorConstants.kRightJoystickPort);
 
   public RobotContainer() {
-    swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(swerveSubsystem, 
-      ()-> leftJoystick.getY(),
-      ()-> -leftJoystick.getX(),
-      ()-> rightJoystick.getX()
-    ));
+    swerveSubsystem.setDefaultCommand(
+        new SwerveJoystickCmd(
+            swerveSubsystem,
+            () -> leftJoystick.getY(),
+            () -> -leftJoystick.getX(),
+            () -> rightJoystick.getX()));
 
-    elevatorSubsystem.setDefaultCommand(new ElevatorCommand(elevatorSubsystem, 
-    () -> xboxController.getLeftY(), 
-    () -> xboxController.getYButton(), 
-    () -> xboxController.getBButton(), 
-    () -> xboxController.getAButton(), 
-    () -> xboxController.getXButton()));
+    elevatorSubsystem.setDefaultCommand(
+        new ElevatorCommand(
+            elevatorSubsystem,
+            () -> xboxController.getLeftY(),
+            () -> xboxController.getYButton(),
+            () -> xboxController.getBButton(),
+            () -> xboxController.getAButton(),
+            () -> xboxController.getXButton()));
 
     configureBindings();
   }
 
   private void configureBindings() {
-    //Controllers need to be added
+    // Controllers need to be added
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/commands/defaultCommands/CoralDispenserCommand.java
+++ src/main/java/frc/robot/commands/defaultCommands/CoralDispenserCommand.java
@@ -1,35 +1,35 @@
 package frc.robot.commands.defaultCommands;
 
-import java.util.function.Supplier;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CoralDispenserSubsystem;
+import java.util.function.Supplier;
 
-public class CoralDispenserCommand extends Command{
-    CoralDispenserSubsystem coralDispenserSubsytem;
-    Supplier<Boolean> xboxControllerRightTrigger;
+public class CoralDispenserCommand extends Command {
+  CoralDispenserSubsystem coralDispenserSubsytem;
+  Supplier<Boolean> xboxControllerRightTrigger;
 
-    public CoralDispenserCommand(CoralDispenserSubsystem coralDispenserSubsytem, Supplier<Boolean> xboxControllerRightTrigger){
-        this.coralDispenserSubsytem = coralDispenserSubsytem;
-        this.xboxControllerRightTrigger = xboxControllerRightTrigger;
-        addRequirements(coralDispenserSubsytem);
-    }
+  public CoralDispenserCommand(
+      CoralDispenserSubsystem coralDispenserSubsytem,
+      Supplier<Boolean> xboxControllerRightTrigger) {
+    this.coralDispenserSubsytem = coralDispenserSubsytem;
+    this.xboxControllerRightTrigger = xboxControllerRightTrigger;
+    addRequirements(coralDispenserSubsytem);
+  }
 
-    @Override
-    public void initialize() {}
+  @Override
+  public void initialize() {}
 
-    @Override
-    public void execute() {
-        if(xboxControllerRightTrigger.get())
-            coralDispenserSubsytem.runMotors(1, -1);
-    }
+  @Override
+  public void execute() {
+    if (xboxControllerRightTrigger.get()) coralDispenserSubsytem.runMotors(1, -1);
+  }
 
-    @Override
-    public void end(boolean interrupted) {}
+  @Override
+  public void end(boolean interrupted) {}
 
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
