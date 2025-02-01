f985455b82ef6adb253ff3125af42bbed3388f72
Charlie Stein <150862516+Charlie-Stein@users.noreply.github.com>
2025-02-01 10:44:57 -0600

    Revert "Merge remote-tracking branch 'origin/addAdvantageKit' into Ben-PathPlanner"
    This reverts commit 5c38012f6d62d34ad6b814b5a7f70aa2e32470b3, reversing
    changes made to f0b0670120bf86ccd182e776e033240542db0d96.

---
+++ src/main/java/frc/robot/subsystems/AlgaeCollectionSubsystem.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.AlgaeConstants;
+import frc.robot.Constants.SensorConstants;
+import frc.robot.Constants.SensorStatus;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+
+public class AlgaeCollectionSubsystem extends SubsystemBase {
+    private SparkMax pivotmotor = new SparkMax(AlgaeConstants.kPivotMotorID, MotorType.kBrushless);
+    private SparkMax spinmotor = new SparkMax(AlgaeConstants.kSpinMotorID, MotorType.kBrushless);
+
+    private DutyCycleEncoder absoluteEncoder = new DutyCycleEncoder(SensorConstants.kAlgeaABSEncoderDIOPort);
+    public AlgaeCollectionSubsystem() {
+        
+      SparkMaxConfig config = new SparkMaxConfig();
+          config.smartCurrentLimit(35);
+          config.idleMode(IdleMode.kBrake);
+      pivotmotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+      spinmotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+      SensorStatus.kPivotAngle = absoluteEncoder.get()*360;//updating it before its read, converting it to degrees as well
+    }
+    public void runPivotMotor(double powerPercent){
+    pivotmotor.set(powerPercent);
+  }
+  public void runSpinMotor(double powerPercent){
+    pivotmotor.set(powerPercent);
+  }
+     @Override
+  public void periodic() {
+    // updating the sensors status to be read by other files
+    SensorStatus.kPivotAngle = absoluteEncoder.get()*360;// converting it to degrees
+  }
+}
