29973216db3e4ca407c9d51572672a3cb1bb66f1
Aiden <114958932+sqeak20000@users.noreply.github.com>
2025-02-04 18:56:30 -0600

    added command

---
+++ src/main/java/frc/robot/commands/defaultCommands/CoralDispenserCommand.java
@@ -0,0 +1,36 @@
+package frc.robot.commands.defaultCommands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralDispenserSubsystem;
+
+public class CoralDispenserCommand extends Command{
+    CoralDispenserSubsystem coralDispenserSubsytem;
+    Supplier<Boolean> xboxControllerRightTrigger;
+
+    public CoralDispenserCommand(CoralDispenserSubsystem coralDispenserSubsytem, Supplier<Boolean> xboxControllerRightTrigger){
+        this.coralDispenserSubsytem = coralDispenserSubsytem;
+        this.xboxControllerRightTrigger = xboxControllerRightTrigger;
+        addRequirements(coralDispenserSubsytem);
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {
+        if(xboxControllerRightTrigger.get())
+            coralDispenserSubsytem.runMotors(1, -1);
+    }
+
+    @Override
+    public void end(boolean interrupted) {}
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
+
