f8cb3afd732aa0e90c0721f1b954432ae7a51952
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2025-02-12 15:16:15 -0600

    uncommenting tests

--- src/test/java/frc/robot/subsystems/TestSwerveSubsystem.java
+++ src/test/java/frc/robot/subsystems/TestSwerveSubsystem.java
@@ -15,7 +15,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.units.measure.Angle;
 
-public class TestSwerveSubsystem {
+public class TestSwerveSubsystem 
     Pigeon2 pigeon;
     //needs to use typeparameter Angle to align with getYaw
     StatusSignal<Angle> yawStatusSignal;
@@ -78,17 +78,17 @@ public class TestSwerveSubsystem {
         targetDrivePos = new Translation2d(1,1);
         assertFalse(swerveSubsystem.isAtPoint(targetDrivePos));
     }
-    // @Test
-    // public void testOdometryAngle(){
-    //     when(yawStatusSignal.getValueAsDouble()).thenReturn(90.0);
+    @Test
+    public void testOdometryAngle(){
+         when(yawStatusSignal.getValueAsDouble()).thenReturn(90.0);
         
 
-    //     when(pitchStatusSignal.getValueAsDouble()).thenReturn(0.0);
-    //     when(rollStatusSignal.getValueAsDouble()).thenReturn(0.0);
-    //     swerveSubsystem.periodic();
-    //     var odometerPostUpdate = swerveSubsystem.getOdometer().getPoseMeters();
-    //     assertEquals(90.0, odometerPostUpdate.getRotation().getDegrees());
-    // }
+         when(pitchStatusSignal.getValueAsDouble()).thenReturn(0.0);
+         when(rollStatusSignal.getValueAsDouble()).thenReturn(0.0);
+         swerveSubsystem.periodic();
+         var odometerPostUpdate = swerveSubsystem.getOdometer().getPoseMeters();
+         assertEquals(90.0, odometerPostUpdate.getRotation().getDegrees());
+    }
     //TODO: testOdometryTranslation
     /*
     @Test
@@ -102,4 +102,4 @@ public class TestSwerveSubsystem {
         assertEquals(0.0, odometerPostUpdate.getTranslation().getY());
     }
     */
-}
+
