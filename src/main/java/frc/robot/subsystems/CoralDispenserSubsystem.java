da7891d3be1f7e09b8054d164c6ebc48fb128e1a
Sqeak <114958932+sqeak20000@users.noreply.github.com>
2025-02-15 12:08:44 -0600

    new TOF sensor, updated librarys (#41)

--- src/main/java/frc/robot/subsystems/CoralDispenserSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralDispenserSubsystem.java
@@ -5,6 +5,8 @@ import frc.robot.Constants.CoralDispenserConstants;
 import frc.robot.Constants.SensorStatus;
 import edu.wpi.first.wpilibj.I2C;
 import frc.robot.VL53L4CD;
+import au.grapplerobotics.LaserCan;
+import au.grapplerobotics.ConfigurationFailedException;
 
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
@@ -18,6 +20,7 @@ public class CoralDispenserSubsystem extends SubsystemBase{
     private SparkMax leftMotor = new SparkMax(CoralDispenserConstants.kLeftMotorID, MotorType.kBrushless);
     private SparkMax rightMotor = new SparkMax(CoralDispenserConstants.kRightMotorID, MotorType.kBrushless);
     //VL53L4CD timeOfFlightSensor = new VL53L4CD(I2C.Port.kOnboard);
+    private LaserCan laserCan = new LaserCan(0);
     
     public CoralDispenserSubsystem(){
 
@@ -29,6 +32,12 @@ public class CoralDispenserSubsystem extends SubsystemBase{
         rightMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         //timeOfFlightSensor.init();
         //timeOfFlightSensor.startRanging();
+        try {
+            laserCan.setRangingMode(LaserCan.RangingMode.SHORT);
+            laserCan.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_20MS);
+        }catch (ConfigurationFailedException e) {
+            System.out.println("Configuration failed" + e);
+        }
     }
     public void runMotors(double leftPower, double rightPower){
         leftMotor.set(leftPower);
@@ -37,6 +46,14 @@ public class CoralDispenserSubsystem extends SubsystemBase{
 
     @Override
     public void periodic() {
+        LaserCan.Measurement measurement = laserCan.getMeasurement();
+        if (measurement != null && measurement.status ==LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+            double distance = measurement.distance_mm;
+            SmartDashboard.putNumber("distance sensor", distance);
+            SensorStatus.kTimeOfFlightDistance = distance;
+        }else{
+            System.out.println("Oh no! The target is not in range, or we can't get a reliable measurement");
+        }
         
         //double temp = timeOfFlightSensor.measure().distanceMillimeters;
         //SensorStatus.kTimeOfFlightDistance = temp;
