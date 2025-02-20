b1286a694947f206055de2a7bf5442b8c4411484
Charlie Stein <150862516+Charlie-Stein@users.noreply.github.com>
2025-02-20 16:19:38 -0600

    changed from pospose2d to char

--- src/main/java/frc/robot/commands/AutoReefCmd.java
+++ src/main/java/frc/robot/commands/AutoReefCmd.java
@@ -16,7 +16,7 @@ public class AutoReefCmd extends Command{
     int T, Q, P, reefLevel;     
     double X, Y; 
     double elevatorHeight;
-    PosPose2d reefLocation; 
+    char reefLocation; 
     Command placeCoralOnReef;
     boolean calculate;
     public AutoReefCmd(CommandSequences commandSequences, CoralDispenserSubsystem coralDispenserSubsystem, ElevatorSubsystem elevatorSubsystem, SwerveSubsystem swerveSubsystem, int reefLevel) {
@@ -28,9 +28,9 @@ public class AutoReefCmd extends Command{
         calculate = true;
         addRequirements(coralDispenserSubsystem, elevatorSubsystem, swerveSubsystem);
     }
-    public AutoReefCmd(CommandSequences commandSequences, CoralDispenserSubsystem coralDispenserSubsystem, ElevatorSubsystem elevatorSubsystem, SwerveSubsystem swerveSubsystem, int reefLevel, PosPose2d reefLocation){
+    public AutoReefCmd(CommandSequences commandSequences, CoralDispenserSubsystem coralDispenserSubsystem, ElevatorSubsystem elevatorSubsystem, SwerveSubsystem swerveSubsystem, int reefLevel, String reefLocation){
         this(commandSequences, coralDispenserSubsystem, elevatorSubsystem, swerveSubsystem, reefLevel);
-        this.reefLocation = reefLocation;
+        this.reefLocation = reefLocation.charAt(0);
         calculate = false;
     }
 
@@ -98,64 +98,64 @@ public class AutoReefCmd extends Command{
     private int findP() {
         if (Q == 2){
             if (T == 3){
-                reefLocation = Constants.ReefConstants.reefK;
+                reefLocation = "K".charAt(0);
                 return 11;
             } else if (T == 2){
-                reefLocation = Constants.ReefConstants.reefL;
+                reefLocation = "L".charAt(0);
                 return 12;
             } else {
-                reefLocation = Constants.ReefConstants.reefA;
+                reefLocation = "A".charAt(0);
                 return 1;
             }
         } else if (Q == 3){
             if (T == 1){
-                reefLocation = Constants.ReefConstants.reefB;
+                reefLocation = "B".charAt(0);
                 return 2;
             } else if (T == 2){
-                reefLocation = Constants.ReefConstants.reefC;
+                reefLocation = "C".charAt(0);
                 return 3;
             } else {
-                reefLocation = Constants.ReefConstants.reefD;
+                reefLocation = "D".charAt(0);
                 return 4;
             }
         } else if (Q == 4){
             if (T == 3){
-                reefLocation = Constants.ReefConstants.reefE;
+                reefLocation = "E".charAt(0);
                 return 5;
             } else if (T == 2){
-                reefLocation = Constants.ReefConstants.reefF;
+                reefLocation = "F".charAt(0);
                 return 6;
             } else {
-                reefLocation = Constants.ReefConstants.reefG;
+                reefLocation = "G".charAt(0);
                 return 7;
             } 
         }else {
             if (T == 1){
-                reefLocation = Constants.ReefConstants.reefH;
+                reefLocation = "H".charAt(0);
                 return 8;
             } else if (T == 2){
-                reefLocation = Constants.ReefConstants.reefI;
+                reefLocation = "I".charAt(0);
                 return 9;
             } else {
-                reefLocation = Constants.ReefConstants.reefJ;
+                reefLocation = "J".charAt(0);
                 return 10;
             }
         }
     }
 
-    private PosPose2d l1Trough(){
+    private char l1Trough(){
         if (P == 1 || P == 2){
-            return Constants.ReefConstants.reefTroughAB;
+            return "R".charAt(0);
         } else if (P == 3 || P == 4){
-            return Constants.ReefConstants.reefTroughCD;
+            return "S".charAt(0);
         } else if (P == 5 || P == 6){
-            return Constants.ReefConstants.reefTroughEF;
+            return "T".charAt(0);
         } else if (P == 7 || P == 8){
-            return Constants.ReefConstants.reefTroughGH;
+            return "U".charAt(0);
         } else if (P == 9 || P == 10){
-            return Constants.ReefConstants.reefTroughIJ;
+            return "V".charAt(0);
         } else {
-            return Constants.ReefConstants.reefTroughKL;
+            return "W".charAt(0);
         }
     }
 }
