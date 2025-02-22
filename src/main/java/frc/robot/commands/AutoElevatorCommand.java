9d5a7c674d76b630c0cac33af509fc04149a8bb6
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2025-02-22 14:18:50 -0600

    maké elevato+r wurk

--- src/main/java/frc/robot/commands/AutoElevatorCommand.java
+++ src/main/java/frc/robot/commands/AutoElevatorCommand.java
@@ -10,23 +10,25 @@ public class AutoElevatorCommand extends Command{
     Timer timer = new Timer();
     ElevatorSubsystem elevatorSubsystem;
     double elevatorSetHeight;
+    MotorPowerController motorPowerController;
     
     public AutoElevatorCommand(ElevatorSubsystem elevatorSubsystem, double elevatorSetHeight) {
         this.elevatorSetHeight = elevatorSetHeight;
         this.elevatorSubsystem = elevatorSubsystem;
+        motorPowerController = new MotorPowerController(0.07, 0.05, 0.2, 1, 1, SensorStatus.kElevatorHeight, 5);
         addRequirements(elevatorSubsystem);
         // these are guessed numbers, they need to be tuned
     }
 
     @Override
     public void initialize() {
-
+        
         timer.reset();// this is broken
     }
 
     @Override
     public void execute() {// nothing to do since the subsystem handles the driving of it
-        
+        elevatorSubsystem.runElevatorMotors(Math.max(Math.min(-motorPowerController.calculate(elevatorSetHeight, SensorStatus.kElevatorHeight), 1), -1)); //negative because up is reverse
     }
 
     public void end(boolean interrupted) {
