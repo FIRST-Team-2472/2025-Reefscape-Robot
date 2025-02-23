c846e34516fe460d7447008ce2d3ea2e8383fa50
smjones1985 <smjones.virj@gmail.com>
2025-02-23 17:42:10 -0600

    Update CommandSequences.java

--- src/main/java/frc/robot/CommandSequences.java
+++ src/main/java/frc/robot/CommandSequences.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map; // ...new import...
+import java.util.HashMap; // ...new import...
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -34,7 +36,8 @@ import frc.robot.subsystems.SwerveSubsystem;
 
 public class CommandSequences {
     PosPose2d[] cageNodes = new PosPose2d[6];
-    PosPose2d[] reefNodes = new PosPose2d[12];
+    Map<Character, PosPose2d> reefNodesMap = new HashMap<>(); // replaced reefNodes
+
     PosPose2d leftHumanPlayer, rightHumanPlayer, middle, rightReefPassage, leftReefPassage;
     PosPose2d processor;
 
@@ -47,18 +50,19 @@ public class CommandSequences {
         cageNodes[4] = simplePose(7.58, 1.898, 180); //Cage Position 5
         cageNodes[5] = simplePose(7.58, 0.794, 180); //Cage on far right from driver POV
 
-        reefNodes[0] = simplePose(3.15, 4.18, 0); //Reef Position A
-        reefNodes[1] = simplePose(3.15, 3.85, 0); //Reef Position B
-        reefNodes[2] = simplePose(3.7, 2.94, 60); //Reef Position C
-        reefNodes[3] = simplePose(3.98, 2.78, 60); //Reef Position D
-        reefNodes[4] = simplePose(5, 2.78, 120); //Reef Position E
-        reefNodes[5] = simplePose(5.3, 2.960, 120); //Reef Position F
-        reefNodes[6] = simplePose(5.8, 3.85, 180); //Reef Position G
-        reefNodes[7] = simplePose(5.8, 4.18, 180); //Reef Position H
-        reefNodes[8] = simplePose(5.3, 5.09, 240); //Reef Position I
-        reefNodes[9] = simplePose(5.01, 5.25, 240); //Reef Position J // was 5, 
-        reefNodes[10] = simplePose(3.99, 5.24, 300); //Reef Position K
-        reefNodes[11] = simplePose(3.69, 5.1, 300); //Reef Position L
+        // Initialize reefNodesMap with corresponding keys.
+        reefNodesMap.put('A', simplePose(3.15, 4.18, 0)); // Reef Position A
+        reefNodesMap.put('B', simplePose(3.15, 3.85, 0)); // Reef Position B
+        reefNodesMap.put('C', simplePose(3.7, 2.94, 60)); // Reef Position C
+        reefNodesMap.put('D', simplePose(3.98, 2.78, 60)); // Reef Position D
+        reefNodesMap.put('E', simplePose(5, 2.78, 120)); // Reef Position E
+        reefNodesMap.put('F', simplePose(5.3, 2.960, 120)); // Reef Position F
+        reefNodesMap.put('G', simplePose(5.8, 3.85, 180)); // Reef Position G
+        reefNodesMap.put('H', simplePose(5.8, 4.18, 180)); // Reef Position H
+        reefNodesMap.put('I', simplePose(5.3, 5.09, 240)); // Reef Position I
+        reefNodesMap.put('J', simplePose(5.01, 5.25, 240)); // Reef Position J
+        reefNodesMap.put('K', simplePose(3.99, 5.24, 300)); // Reef Position K
+        reefNodesMap.put('L', simplePose(3.69, 5.1, 300)); // Reef Position L
 
         rightHumanPlayer = simplePose(1.127, 0.962, 54);
         leftHumanPlayer = simplePose(1.097, 6.991, 306);
@@ -70,13 +74,13 @@ public class CommandSequences {
     }
     public Command driveAndSitFromMiddle(SwerveSubsystem swerveSubsystem){
         swerveSubsystem.setOdometry(middle.toFieldPose2d());
-        return new SwerveDriveToPointCmd(swerveSubsystem, reefNode('G'));
+        return new SwerveDriveToPointCmd(swerveSubsystem, reefNodesMap.get('G'));
     }
     
     public Command driveAndPlaceOneFromMiddle(SwerveSubsystem swerveSubsystem, ElevatorSubsystem elevatorSubsystem, CoralDispenserSubsystem coralDispenserSubsystem){
         swerveSubsystem.setOdometry(middle.toFieldPose2d());
         return new SequentialCommandGroup(
-            new SwerveDriveToPointCmd(swerveSubsystem, reefNode('G')),
+            new SwerveDriveToPointCmd(swerveSubsystem, reefNodesMap.get('G')),
             new AutoElevatorCommand(elevatorSubsystem, ElevatorConstants.kElevatorL4Height),
             new AutoCoralDispenseCommand(coralDispenserSubsystem)
         );
@@ -84,7 +88,7 @@ public class CommandSequences {
     public Command driveAndPlaceOneFromLeft(SwerveSubsystem swerveSubsystem, ElevatorSubsystem elevatorSubsystem, CoralDispenserSubsystem coralDispenserSubsystem){
         swerveSubsystem.setOdometry(cageNodes[0].toFieldPose2d());
         return new SequentialCommandGroup(
-            new SwerveDriveToPointCmd(swerveSubsystem, reefNode('J')),
+            new SwerveDriveToPointCmd(swerveSubsystem, reefNodesMap.get('J')),
             new AutoElevatorCommand(elevatorSubsystem, ElevatorConstants.kElevatorL4Height),
             new AutoCoralDispenseCommand(coralDispenserSubsystem),
             new AutoElevatorCommand(elevatorSubsystem, 0)
@@ -93,7 +97,7 @@ public class CommandSequences {
     public Command driveAndPlaceOneFromRight(SwerveSubsystem swerveSubsystem, ElevatorSubsystem elevatorSubsystem, CoralDispenserSubsystem coralDispenserSubsystem){
         swerveSubsystem.setOdometry(cageNodes[5].toFieldPose2d());
         return new SequentialCommandGroup(
-            new SwerveDriveToPointCmd(swerveSubsystem, reefNode('E')),
+            new SwerveDriveToPointCmd(swerveSubsystem, reefNodesMap.get('E')),
             new AutoElevatorCommand(elevatorSubsystem, ElevatorConstants.kElevatorL4Height),
             new AutoCoralDispenseCommand(coralDispenserSubsystem),
             new AutoElevatorCommand(elevatorSubsystem, 0)
@@ -102,13 +106,13 @@ public class CommandSequences {
     public Command driveAndPlaceTwoFromLeft(SwerveSubsystem swerveSubsystem, ElevatorSubsystem elevatorSubsystem, CoralDispenserSubsystem coralDispenserSubsystem){
         swerveSubsystem.setOdometry(cageNodes[0].toFieldPose2d());
         return new SequentialCommandGroup(
-            new SwerveDriveToPointCmd(swerveSubsystem, reefNode('J')),
+            new SwerveDriveToPointCmd(swerveSubsystem, reefNodesMap.get('J')),
             new AutoElevatorCommand(elevatorSubsystem, ElevatorConstants.kElevatorL4Height),
             new AutoCoralDispenseCommand(coralDispenserSubsystem),
             new AutoElevatorCommand(elevatorSubsystem, 0),
             new SwerveFollowTransitionCmd(swerveSubsystem, leftReefPassage, leftHumanPlayer, 1),
             new CollectCoralCmd(coralDispenserSubsystem),// this command is missing stuff
-            new SwerveDriveToPointCmd(swerveSubsystem, reefNode('L')),
+            new SwerveDriveToPointCmd(swerveSubsystem, reefNodesMap.get('L')),
             new AutoElevatorCommand(elevatorSubsystem, ElevatorConstants.kElevatorL4Height),
             new AutoCoralDispenseCommand(coralDispenserSubsystem),
             new AutoElevatorCommand(elevatorSubsystem, 0)
@@ -117,13 +121,13 @@ public class CommandSequences {
     public Command driveAndPlaceTwoFromRight(SwerveSubsystem swerveSubsystem, ElevatorSubsystem elevatorSubsystem, CoralDispenserSubsystem coralDispenserSubsystem){
         swerveSubsystem.setOdometry(cageNodes[0].toFieldPose2d());
         return new SequentialCommandGroup(
-            new SwerveDriveToPointCmd(swerveSubsystem, reefNode('E')),
+            new SwerveDriveToPointCmd(swerveSubsystem, reefNodesMap.get('E')),
             new AutoElevatorCommand(elevatorSubsystem, ElevatorConstants.kElevatorL4Height),
             new AutoCoralDispenseCommand(coralDispenserSubsystem),
             new AutoElevatorCommand(elevatorSubsystem, 0),
             new SwerveFollowTransitionCmd(swerveSubsystem, rightReefPassage, rightHumanPlayer, 1),
             new CollectCoralCmd(coralDispenserSubsystem),// this command is missing stuff
-            new SwerveDriveToPointCmd(swerveSubsystem, reefNode('C')),
+            new SwerveDriveToPointCmd(swerveSubsystem, reefNodesMap.get('C')),
             new AutoElevatorCommand(elevatorSubsystem, ElevatorConstants.kElevatorL4Height),
             new AutoCoralDispenseCommand(coralDispenserSubsystem),
             new AutoElevatorCommand(elevatorSubsystem, 0)
@@ -138,7 +142,7 @@ public class CommandSequences {
         return new SequentialCommandGroup(
 
             //new ParallelCommandGroup(
-                new SwerveDriveToPointCmd(swerveSubsystem, reefNodes[0]),
+                new SwerveDriveToPointCmd(swerveSubsystem, reefNodesMap.get('A')),
                 //new AutoElevatorCommand(elevatorSubsystem, ElevatorConstants.kElevatorL4Height)
             //),
             //new AutoCoralDispenseCommand(coralDispenserSubsystem),
@@ -148,7 +152,7 @@ public class CommandSequences {
             ),
             new SwerveDriveToPointCmd(swerveSubsystem, leftHumanPlayer),
             new ParallelCommandGroup(
-                //new SwerveDriveToPointCmd(swerveSubsystem, reefNodes[4])
+                //new SwerveDriveToPointCmd(swerveSubsystem, reefNodesMap.get('E'))
                 //new AutoElevatorCommand(elevatorSubsystem, ElevatorConstants.kElevatorL4Height)
             ),
             new AutoCoralDispenseCommand(coralDispenserSubsystem)
@@ -168,38 +172,6 @@ public class CommandSequences {
         swerveSubsystem.calibrateOdometry(0.0f);
         Pose2d currentPos = swerveSubsystem.getOdometer().getPoseMeters();
         return new SwerveDriveToPointCmd(swerveSubsystem, simplePose(currentPos.getX() - 0.9f, currentPos.getY(), currentPos.getRotation().getDegrees()));
-    }
-  
-    public PosPose2d reefNode(char NodeLetter){
-        switch (NodeLetter) {
-            case 'A':
-                return reefNodes[0];
-            case 'B':
-                return reefNodes[1];
-            case 'C':  
-                return reefNodes[2];
-            case 'D':
-                return reefNodes[3];
-            case 'E':
-                return reefNodes[4];
-            case 'F':
-                return reefNodes[5];
-            case 'G':
-                return reefNodes[6];
-            case 'H':
-                return reefNodes[7];
-            case 'I':
-                return reefNodes[8];
-            case 'J':
-                return reefNodes[9];
-            case 'K':
-                return reefNodes[10];
-            case 'L':
-                return reefNodes[11];
-            default:
-                return reefNodes[0];
-        }
-
     }
 
     public PosPose2d simplePose(double x, double y, double angleDegrees) {
