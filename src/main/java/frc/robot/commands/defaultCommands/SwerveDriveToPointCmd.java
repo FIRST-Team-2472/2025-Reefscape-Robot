51da3943262761362b366e8937408a4b49ba3509
smjones1985 <smjones.virj@gmail.com>
2025-02-23 20:24:49 -0600

    logging enhancements

--- src/main/java/frc/robot/commands/defaultCommands/SwerveDriveToPointCmd.java
+++ src/main/java/frc/robot/commands/defaultCommands/SwerveDriveToPointCmd.java
@@ -34,8 +34,8 @@ public class SwerveDriveToPointCmd extends Command {
   @Override
   public void initialize() {
     swerveSubsystem.initializeDriveToPointAndRotate(targetPosition);
-    //System.out.println("robot pose" + swerveSubsystem.getPose().getX()+", "+ swerveSubsystem.getPose().getY());
-    //System.out.println("target pose" + targetPosition.getX()+", "+ targetPosition.getY());
+    RobotLogManager.debug("robot pose" + swerveSubsystem.getPose().getX()+", "+ swerveSubsystem.getPose().getY());
+    RobotLogManager.debug("target pose" + targetPosition.getX()+", "+ targetPosition.getY());
     timer.restart();
   }
 
@@ -52,11 +52,13 @@ public class SwerveDriveToPointCmd extends Command {
   @Override
   public boolean isFinished() {
     if (swerveSubsystem.isExactlyInPosition(targetPosition) || swerveSubsystem.isNearlyInPosition(targetPosition) || swerveSubsystem.isStalling()){
-      RobotLogManager.info("Finished Driving");
+      RobotLogManager.info("Successfully finished Driving - " + "robot pose" + swerveSubsystem.getPose().getX()+", "+ swerveSubsystem.getPose().getY());
       return true;
     }
-    if(timer.hasElapsed(3))
+    if(timer.hasElapsed(3)){
+      RobotLogManager.debug("Timed out - ending pose" + targetPosition.getX()+", "+ targetPosition.getY());
       return true;
+    }
 
     return false;
   }
