9d0d3fe79d13a081c49361d58840ba7432710b3d
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2025-02-28 12:52:18 -0600

    Added Limelights

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -443,18 +443,18 @@ public class SwerveSubsystem extends SubsystemBase {
         // Pose2d filteredBotPose = getFilteredPose();
         // SmartDashboard.putNumber("Filtered Pose X", filteredBotPose.getX());
         // SmartDashboard.putNumber("Filtered Pose Y", filteredBotPose.getY());
-        /*
-         * try {
-         * if (periods == 0) {
-         * calibrateOdometry();
-         * periods = 10;
-         * }
-         * 
-         * periods--;
-         * } catch (Exception NullPointerException) {
-         * // TODO: handle exception
-         * }
-         */
+        
+        try {
+         if (periods == 0) {
+         calibrateOdometry();
+         periods = 10;
+        }
+        
+         periods--;
+        } catch (Exception NullPointerException) {
+         // TODO: handle exception
+        }
+         
 
         SmartDashboard.putNumber("frontLeft Encoder",
                 frontLeft.absoluteEncoder.getAbsolutePosition().getValueAsDouble());
