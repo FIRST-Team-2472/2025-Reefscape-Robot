cd2dca2d90d16ba03521d77863d3c4eaa44e97ef
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2025-03-25 20:01:54 -0500

    Updates to coral and stuff

--- src/main/java/frc/robot/commands/CollectCoralCmd.java
+++ src/main/java/frc/robot/commands/CollectCoralCmd.java
@@ -1,25 +1,38 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CoralDispenserSubsystem;
 
 public class CollectCoralCmd extends Command{
+
+    Timer timer;
+
     CoralDispenserSubsystem coralDispenserSubsystem;
     public CollectCoralCmd(CoralDispenserSubsystem coralDispenserSubsystem){
         addRequirements(coralDispenserSubsystem);
         this.coralDispenserSubsystem = coralDispenserSubsystem;
 
+        timer = new Timer();
+
     }
     @Override
     public void initialize() {
+        timer.restart();
     }
     @Override
     public void execute() {
-        coralDispenserSubsystem.runMotors(.5, -.5);
+        if (coralDispenserSubsystem.seecoral == true){
+            coralDispenserSubsystem.runMotors(.5, -.5);
+        }
     }
     @Override
     public void end(boolean interrupted) {
-        coralDispenserSubsystem.runMotors(0,0);
+        if (timer.hasElapsed(1) && coralDispenserSubsystem.seecoral == false){
+            coralDispenserSubsystem.runMotors(0,0);
+        } else {
+            execute();
+        }
     }
     @Override
     public boolean isFinished() {
