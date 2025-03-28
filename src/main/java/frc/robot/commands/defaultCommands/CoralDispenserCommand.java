24c108815386763930516507f84c9f9c2ad457bf
Aiden <114958932+sqeak20000@users.noreply.github.com>
2025-03-28 13:08:02 -0500

    added delay to intaking coral

--- src/main/java/frc/robot/commands/defaultCommands/CoralDispenserCommand.java
+++ src/main/java/frc/robot/commands/defaultCommands/CoralDispenserCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands.defaultCommands;
 
 import java.util.function.Supplier;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.SensorStatus;
 import frc.robot.subsystems.CoralDispenserSubsystem;
@@ -9,12 +10,15 @@ import frc.robot.subsystems.CoralDispenserSubsystem;
 public class CoralDispenserCommand extends Command{
     CoralDispenserSubsystem coralDispenserSubsystem;
     Supplier<Double> xboxControllerRightTrigger, xboxControllerLeftTrigger;
+    Timer CollectionDelay;
+    double delay = .1;
 
     public CoralDispenserCommand(CoralDispenserSubsystem coralDispenserSubsystem, Supplier<Double> xboxControllerRightTrigger, Supplier<Double> xboxControllerLeftTrigger){
         this.coralDispenserSubsystem = coralDispenserSubsystem;
         this.xboxControllerRightTrigger = xboxControllerRightTrigger;
         this.xboxControllerLeftTrigger = xboxControllerLeftTrigger;
         addRequirements(coralDispenserSubsystem);
+        CollectionDelay = new Timer();
     }
 
     @Override
@@ -29,9 +33,14 @@ public class CoralDispenserCommand extends Command{
             }
             else if (SensorStatus.kElevatorHeight < 3) {
                 if (!coralDispenserSubsystem.hascoral) {
-                    coralDispenserSubsystem.runMotors(.5, -.5);
+                    coralDispenserSubsystem.runMotors(.3, -.3);
                 } else {
-                    coralDispenserSubsystem.runMotors(0, 0);
+                    if(CollectionDelay.hasElapsed(delay))
+                        coralDispenserSubsystem.runMotors(0, 0);
+                    else{
+                        coralDispenserSubsystem.runMotors(.3, -.3);
+                        CollectionDelay.start();
+                    }
                 }
             }
             else {
@@ -41,8 +50,12 @@ public class CoralDispenserCommand extends Command{
         }else if(xboxControllerLeftTrigger.get() > 0.3) {
             coralDispenserSubsystem.runMotors(-.3, .3);
             coralDispenserSubsystem.hascoral = false;
-        }else
+        }else{
             coralDispenserSubsystem.runMotors(0, 0);
+            CollectionDelay.reset();
+            CollectionDelay.stop();
+        }
+            
         
     }
 
