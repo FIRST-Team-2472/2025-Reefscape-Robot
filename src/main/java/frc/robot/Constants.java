3f21117d588cfb7f0ff245ca127343c1cc83a4ac
Aiden <114958932+sqeak20000@users.noreply.github.com>
2025-03-29 12:53:01 -0500

    reverted dimensions

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -223,7 +223,7 @@ public final class Constants {
     public static final class SensorConstants {
         public static final int kPigeonID = 0;
 
-        public static final double sizeOfFieldMetersX = 17.5;//17.548225;
+        public static final double sizeOfFieldMetersX = 17.548225;
         public static final double sizeOfFieldMetersY = 8.0518;
 
         public static int kAlgeaABSEncoderDIOPort = 0; // set later
