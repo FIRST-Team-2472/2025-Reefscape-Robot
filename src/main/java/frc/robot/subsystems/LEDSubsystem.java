0b7971ba88c995f501cfceb4b13be1e286c7536b
sqeak20000 <astegora@gmail.com>
2025-04-16 18:55:30 -0500

    import cleanup

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,19 +1,13 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.units.Unit;
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.LEDPattern;
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotStatus;
 
-import static edu.wpi.first.units.Units.*;
 import static frc.robot.Constants.LEDConstants.*;
 
 import java.util.Map;
