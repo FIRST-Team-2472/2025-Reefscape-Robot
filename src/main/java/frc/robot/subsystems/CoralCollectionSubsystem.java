abc1af676535c6dace7440c1795d3debec3a37dd
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2025-05-08 16:18:53 -0500

    fix servo angle

--- src/main/java/frc/robot/subsystems/CoralCollectionSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralCollectionSubsystem.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class CoralCollectionSubsystem extends SubsystemBase{
     Servo intakeServo = new Servo(9);
     public CoralCollectionSubsystem() {
-        intakeServo.setAngle(110);
+        intakeServo.setAngle(130);
     }
     public void setServoAngle(double angle){
         intakeServo.setAngle(angle);
