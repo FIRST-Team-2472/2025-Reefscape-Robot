43735b7b3f7962459a269edb3b0bf117cec5e7b5
Sqeak <114958932+sqeak20000@users.noreply.github.com>
2026-01-08 15:49:45 -0600

    has fake data

--- src/main/java/frc/robot/extras/LidarMap.java
+++ src/main/java/frc/robot/extras/LidarMap.java
@@ -3,56 +3,47 @@ package frc.robot.extras;
 import frc.robot.extras.LidarMapComponents.Polygon;
 
 public class LidarMap {
-    public Polygon[] map = new Polygon[3];
+    public Polygon[] map = new Polygon[2];
 
     public LidarMap() {//all numbers are magic so far
         // Define polygons and add them to the map array
-        // Polygon 1 aka the field walls
+        // Polygon 1 aka the field walls 
+        //currently just a 10x10 square
         map[0] = new Polygon(new frc.robot.extras.LidarMapComponents.LineSegment[] {
                 new frc.robot.extras.LidarMapComponents.LineSegment(
                         new frc.robot.extras.LidarMapComponents.MapPoint(0, 0),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(100, 0)),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(10, 0)),
                 new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(100, 0),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(100, 100)),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(10, 0),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(10, 10)),
                 new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(100, 100),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(0, 100)),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(10, 10),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(0, 10)),
                 new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(0, 100),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(0, 10),
                         new frc.robot.extras.LidarMapComponents.MapPoint(0, 0))
         });
 
-        // Polygon 2
+        // Polygon aka hexagon in the middle
         map[1] = new Polygon(new frc.robot.extras.LidarMapComponents.LineSegment[] {
                 new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(150, 150),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(250, 150)),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(6.000, 5.577),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(5.000, 6.155)),
                 new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(250, 150),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(250, 250)),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(5.000, 6.155),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(4.000, 5.577)),
                 new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(250, 250),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(150, 250)),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(4.000, 5.577),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(4.000, 4.423)),
                 new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(150, 250),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(150, 150))
-        });
-
-        // Polygon 3
-        map[2] = new Polygon(new frc.robot.extras.LidarMapComponents.LineSegment[] {
+                        new frc.robot.extras.LidarMapComponents.MapPoint(4.000, 4.423),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(5.000, 3.845)),
                 new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(300, 300),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(400, 300)),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(5.000, 3.845),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(6.000, 4.423)),
                 new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(400, 300),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(400, 400)),
-                new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(400, 400),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(300, 400)),
-                new frc.robot.extras.LidarMapComponents.LineSegment(
-                        new frc.robot.extras.LidarMapComponents.MapPoint(300, 400),
-                        new frc.robot.extras.LidarMapComponents.MapPoint(300, 300))
+                        new frc.robot.extras.LidarMapComponents.MapPoint(6.000, 4.423),
+                        new frc.robot.extras.LidarMapComponents.MapPoint(6.000, 5.577))
         });
     }
 }
