8fce1b6e3a63aba8b7ff11b831c95b90978e326e
Sqeak <114958932+sqeak20000@users.noreply.github.com>
2026-01-10 10:41:12 -0600

    rough generation done

--- src/main/java/frc/robot/extras/PointCloudPositionEstimator.java
+++ src/main/java/frc/robot/extras/PointCloudPositionEstimator.java
@@ -4,17 +4,39 @@ import frc.robot.extras.LidarMapComponents.MapPoint;
 
 public class PointCloudPositionEstimator {
     public static FieldPoint estimatePose(FieldPose2d roughPose, double[] pointCloud){
+        MapPoint[] roughParticles = generateParticles(roughPose);
+        return null;
+    }
+
+    
+    public static MapPoint[] generateParticles(FieldPose2d roughPose){
         MapPoint[] roughParticles = new MapPoint[10];
         for(int i = 0; i < roughParticles.length; i++){
             roughParticles[i] = new MapPoint(
                 roughPose.getX() + Math.random() * 0.05 - 0.025,// 5cm tolerance to start
                 roughPose.getY() + Math.random() * 0.05 - 0.025
             );
-
+            while(isTooClose(roughParticles[i], roughParticles, (short)i)){// regenerarte if too close to previous points
+                roughParticles[i] = new MapPoint(
+                    roughPose.getX() + Math.random() * 0.05 - 0.025,
+                    roughPose.getY() + Math.random() * 0.05 - 0.025
+                );
+            }
         }
-        return null;
+        return roughParticles;
     }
-    public static boolean isTooClose(MapPoint )
+    public static boolean isTooClose(MapPoint pointToCheck, MapPoint[] PreviousPoints, short arrayPosition){
+        for(int i = 0; i < arrayPosition; i++){
+            double distance = Math.sqrt(
+                (pointToCheck.x - PreviousPoints[i].x)*(pointToCheck.x - PreviousPoints[i].x) +
+                (pointToCheck.y - PreviousPoints[i].y)*(pointToCheck.y - PreviousPoints[i].y)
+            );
+            if(distance < 0.01){ //5mm tolerance for duplicate points
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args){
 
     }
